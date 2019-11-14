31fc25808ec2a26eecd92acbb943f6073d03d048
Roman Jaquez <romanjaquez@Romans-MacBook-Pro.local>
2019-11-14 00:04:52 -0500

    changes for approval

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Applications/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/romanjaquez/Documents/Flutter/gdglawrence-app"
+export "FLUTTER_TARGET=/Users/romanjaquez/Documents/Flutter/gdglawrence-app/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Applications/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=2.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
